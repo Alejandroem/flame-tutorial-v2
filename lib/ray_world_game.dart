67f96ca40c1046a3ca9f17474ddbb3efc45985d2
Alejandro <alexalejandroem@gmail.com>
2021-10-27 16:34:13 +0200

    fixed error on initial positioN

--- lib/ray_world_game.dart
+++ lib/ray_world_game.dart
@@ -10,15 +10,16 @@ class RayWorldGame extends FlameGame {
   final World _world = World();
 
   @override
-  Future<void> onLoad() async {
+  Future<void>? onLoad() async {
     await add(_world);
     add(_player);
+    _player.position = _world.size / 2;
+    camera.followComponent(_player,
+        worldBounds: Rect.fromLTRB(0, 0, _world.size.x, _world.size.y));
+    return super.onLoad();
   }
 
   void onJoypadDirectionChanged(Direction direction) {
     _player.direction = direction;
-    _player.position = _world.size / 2;
-    camera.followComponent(_player,
-        worldBounds: Rect.fromLTRB(0, 0, _world.size.x, _world.size.y));
   }
 }
