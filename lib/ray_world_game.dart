6a78e3c478d0c24cb5839fd3ffc97fe81833ae21
Alejandro <alexalejandroem@gmail.com>
2021-10-27 16:26:08 +0200

    adding world

--- lib/ray_world_game.dart
+++ lib/ray_world_game.dart
@@ -1,12 +1,15 @@
 import 'package:flame/game.dart';
 import 'components/player.dart';
+import 'components/world.dart';
 import 'helpers/direction.dart';
 
 class RayWorldGame extends FlameGame {
   final Player _player = Player();
+  final World _world = World();
 
   @override
   Future<void> onLoad() async {
+    await add(_world);
     add(_player);
   }
 
